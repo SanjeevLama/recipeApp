f4fa5a270200026f32cbf458b9b52714d3049e89
SanjeevLama <sanjeevghisinglama@gmail.com>
2025-09-21 13:58:25 +0545

    FIX: Correct paths in build script

--- backEnd/build.sh
+++ backEnd/build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# Change directory into the folder containing manage.py
+cd recipeApp
 
+# Now that we are in the correct directory, run the commands
+pip install -r requirements.txt
 python manage.py migrate
